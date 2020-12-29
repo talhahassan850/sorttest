760176b3b9d236d478613f2dcad1f59616070a00
talhahassan850 <72450291+talhahassan850@users.noreply.github.com>
2020-12-29 13:05:15 +0500

    Update genericService.js

--- src/components/genericService.js
+++ src/components/genericService.js
@@ -2,7 +2,6 @@ import axios from "axios";
 //axios.defaults.baseURL = "https://restfulapi29.herokuapp.com/api/product/";
 axios.defaults.baseURL = "https://restfulapi29.herokuapp.com/api/";
 //axios.defaults.baseURL = "http://localhost:4000/api/";
-//axios.defaults.baseURL = "mongodb://talhahassan:talhahassan@cluster0-shard-00-00.bbwhb.mongodb.net:27017,cluster0-shard-00-01.bbwhb.mongodb.net:27017,cluster0-shard-00-02.bbwhb.mongodb.net:27017/api?ssl=true&replicaSet=atlas-11f2ie-shard-0&authSource=admin&retryWrites=true&w=majority";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 class GenericService {
   constructor() {}
